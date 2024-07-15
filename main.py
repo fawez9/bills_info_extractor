37af98109e65230345d4ab5f0cf2fbea863509af
fawez9 <hattabifawez99@gmail.com>
2024-07-15 11:39:41 +0100

    updated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.extractor import chatbot
 
 
 if __name__ == "__main__":
-    file_path = input("Enter file path: ")
-    user_query = input("what do you want to extract: ")
+    # just for testing
+    """ file_path = input("Enter file path: ")
+    user_query = input("what do you want to extract: ") """
     chatbot(file_path, user_query)
